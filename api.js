ef60af5b755d07c7e86eabe51058e93fe72a1477
vitor <vitor.petro2@gmail.com>
2022-05-19 21:32:08 -0300

    conclusão

--- api.js
+++ api.js
@@ -1,7 +1,7 @@
 function buscaUsuario() {
     const userName = $("#name").val();
     const url = `https://api.github.com/users/${userName}`;
-    const urlRepos= `https://api.github.com/users/${userName}/repos`;
+    const urlRepos = `https://api.github.com/users/${userName}/repos`;
     const sort = $("#sort_repos").val();
     const direction = $("#direction_repos").val();
     console.log(sort);
@@ -9,12 +9,12 @@ function buscaUsuario() {
     console.log(userName);
 
     $.ajax({
-        type:'GET',
-        url: url, 
-        success: function(response){
-           console.log(response) 
-            $('#usuario').css('display','block');      
-            $('#usuario_n_encontrado').css('display','none');
+        type: 'GET',
+        url: url,
+        success: function (response) {
+            console.log(response)
+            $('#usuario').css('display', 'block');
+            $('#usuario_n_encontrado').css('display', 'none');
 
             $('#usuario_numSeguidores').text(response.followers);
             $('#usuario_numSeguidos').text(response.following);
@@ -22,42 +22,42 @@ function buscaUsuario() {
             $('#E-mail').text(response.email_usuario);
             $('#Bio').text(response.bio_usuario);
         },
-        error:function(){
-           $('#usuario_n_encontrado').text("Não foi encontrado");           
-           $('#usuario_n_encontrado').css('display','block');            
+        error: function () {
+            alert("Digite um perfil válido")
+            $('#usuario_n_encontrado').css('display', 'block');
         }
     })
-    
+
     $.ajax({
-        type:'GET',
-        url: urlRepos, 
-        data:{
+        type: 'GET',
+        url: urlRepos,
+        data: {
             sort: sort,
-            direction:direction
+            direction: direction
         },
-        success: function(response){
+        success: function (response) {
             console.log(response)
             $("#lista_repositorios").text("");
-            
-            var div  = "<br><label>Nome do Repositório: </label>";
-                div += "<p id='nome_repos_[[i]]'>[[value_name]]</p>";
-                div += "<p id='views_repos_[[i]]'>[[value_views_count]]</p>";
-                div += "<a id='views_repos_[[i]]' href='./detalhes.html?full_name=[[value_full_name]]'>Veja mais</a>";
-                
 
-            
-            $.each( response, function( key, value ) {
-               var eachDiv = div.replaceAll("[[i]]",key);
-                   eachDiv = eachDiv.replace("[[value_name]]", value.name);
-                   eachDiv = eachDiv.replace("[[value_views_count]]", value.watchers_count);
-                   eachDiv = eachDiv.replace("[[value_full_name]]", value.full_name);
-               $("#lista_repositorios").append(eachDiv);
+            var div = "<br><label>Nome do Repositório: </label>";
+            div += "<p id='nome_repos_[[i]]'>[[value_name]]</p>";
+            div += "<p id='views_repos_[[i]]'>[[value_views_count]]</p>";
+            div += "<a id='views_repos_[[i]]' href='./detalhes.html?full_name=[[value_full_name]]'>Veja mais</a>";
+
+
+
+            $.each(response, function (key, value) {
+                var eachDiv = div.replaceAll("[[i]]", key);
+                eachDiv = eachDiv.replace("[[value_name]]", value.name);
+                eachDiv = eachDiv.replace("[[value_views_count]]", value.watchers_count);
+                eachDiv = eachDiv.replace("[[value_full_name]]", value.full_name);
+                $("#lista_repositorios").append(eachDiv);
             });
         }
-    })  
-   
-    
-} 
+    })
+
+
+}
 
 
 
@@ -77,27 +77,27 @@ console.log(myParam)
 function buscaRepositorio() {
 
 
-     const urlFullName= `https://api.github.com/repos/${myParam}`; 
-   
+    const urlFullName = `https://api.github.com/repos/${myParam}`;
+
 
     $.ajax({
-        type:'GET',
-        url: urlFullName, 
-        success: function(response){
-           console.log(response) 
-           
+        type: 'GET',
+        url: urlFullName,
+        success: function (response) {
+            console.log(response)
+
 
             $('#nome_repositorio').text(response.name);
-            $('#descricao_repositorio').val(response.description);
+            $('#descricao_repositorio').text(response.description);
             $('#link_repositorio').attr('href', response.html_url);
-            $('#numero_de_estrelas_repositorio').val(response.stargazers_count);
-            $('#linguagem_repositorio').val(response.language);
+            $('#numero_de_estrelas_repositorio').text(response.stargazers_count);
+            $('#linguagem_repositorio').text(response.language);
         },
-        error:function(){
-           $('#respositotio_n_encrontrado').text("Não foi encontrado");           
-           $('#respositotio_n_encrontrado').css('display','block');            
+        error: function () {
+            $('#respositotio_n_encrontrado').text("Não foi encontrado");
+            $('#respositotio_n_encrontrado').css('display', 'block');
         }
-      })
+    })
 }
 
 
